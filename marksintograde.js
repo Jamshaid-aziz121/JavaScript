5924c1a151d051ac21a244a686d20729dbd4c169
Jamez-1 <jamshaid.aziz121@gmail.com>
2022-12-12 00:14:56 +0500

    modified marksintograde"

--- marksintograde.js
+++ marksintograde.js
@@ -1,33 +1,36 @@
 var obtainMarks = +prompt("Insert your total marks");
 var outofMark = +prompt(" out of marks");
 var result =  obtainMarks/outofMark * 100;
-console.log(result);
+console.log("Your Percentage"  + result);
 
-// var result = +prompt("enter your number 1", '00');
+// var result = +prompt("enter your number ", '00');
+
+var remarks = "You are"
+var remarks2 = "your Grade is :  "
 
 if (result>100){
 
     console.log("Enter your correct number");
 }
 else if (result>=90 && result<100) {
-    console.log("Excellent");
+    console.log(remarks +  " Excellent ");
 }
 
 else if (result>=80 && result<90) {
-    console.log("Brilliant");
+    console.log(remarks + " Brilliant");
 }
 
 else if (result>=70 && result<80) {
-    console.log("A Grade");
+    console.log(remarks2 + " A ");
 }
 
 else if (result>=60 && result<70) {
-    console.log("B Grade");
+    console.log(remarks2 + " B ");
 }
 
 else if (result>=50 && result<60) {
-    console.log("C Grade");
+    console.log(remarks2 + " C ");
 }
 else {
-    console.log("Fail");
+    console.log(reamrks + " Fail");
 }
